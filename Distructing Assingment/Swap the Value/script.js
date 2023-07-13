a7587686bddd0b8d009c0b40f8ca30bb52877048
Shoaib Alam <124783255+Shoaib-Alam-Ansari@users.noreply.github.com>
2023-07-13 22:22:25 +0530

    Add files via upload

--- Distructing Assingment/Swap the Value/script.js
+++ Distructing Assingment/Swap the Value/script.js
@@ -1,13 +1,25 @@
 
-function swap (a, b){
-    [a, b] = [b, a]
+// function swap (a, b){
+//     [a, b] = [b, a]
 
-    return [a, b]
-}
+//     return [a, b]
+// }
 
-let x = 1;
-let y = 2;
+// let x = 1;
+// let y = 2;
 
-[x, y] = swap(x, y)
+// [x, y] = swap(x, y)
 
-console.log(x, y);
+// console.log(x, y);
+
+
+
+let a = 3;
+let b = 4;
+
+a = a + b;
+b = a - b
+a = a - b
+
+console.log(a);
+console.log(b);
